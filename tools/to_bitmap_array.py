8f037f7c8f6249dc663b1d894192cc4bdc3d6016
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-03-07 14:05:35 +0100

    add processing

--- tools/to_bitmap_array.py
+++ tools/to_bitmap_array.py
@@ -1,7 +1,7 @@
 """
-Convert images to bitmap
-Input: stdin (pipe the image into this script)
-Output: stdout
+Convert images to bitmap C-like array, after some pre-processing (grayscale and resize)
+Input: stdin - pipe the image into this script
+Output: stdout - C-like array of bytes
 """
 
 __authors__ = "5H wild nerds"
@@ -11,9 +11,33 @@ from io import BytesIO
 from PIL import Image
 
 def main() -> None:
-    stdin_img_bytes = BytesIO(sys.stdin.buffer.read())
-    img = Image.open(stdin_img_bytes).convert('LA')  # Convert to grayscale
-    img.show()
+    """
+    Get input image as raw bytes
+    """
+    stdin_img_bytes: ByteIO = BytesIO(sys.stdin.buffer.read())
+    
+    """
+    Processing
+    """
+    img: Image = Image.open(stdin_img_bytes)
+    img: Image = img.convert('LA')  # Convert to grayscale
+    img: Image = img.resize((128, 64), Image.LANCZOS)  # Resize to 128x64 or 128x62 [pixels]. Lanczos interpolation for best result
+    
+    """
+    Saving image into ByteIO object
+    """
+    img_byte_array: BytesIO = BytesIO()
+    img.save(img_byte_array, format = 'png')
+    
+    """
+    Getting bytes (array of bytes)
+    """
+    stdout_img_bytes: bytes = img_byte_array.getvalue()
+
+    """
+    Printing the array of bytes as C-like array
+    """
+    print([hex(byte) for byte in stdout_img_bytes])
 
 if __name__ == "__main__":
     main()
