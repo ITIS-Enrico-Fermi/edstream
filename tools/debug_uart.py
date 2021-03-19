e0651a40f046bb1ee33a89e33638254234152bdb
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2021-03-19 18:42:38 +0100

    Added speed test/block send mode

--- tools/debug_uart.py
+++ tools/debug_uart.py
@@ -9,13 +9,23 @@ __authors__ = "5H wild nerds"
 
 import serial
 import argparse
+import threading
+import logging
 
 def send_raw_byte(port: serial.Serial, hex_repr: str):
     x = bytes.fromhex(hex_repr)
     port.write(x)
 
-def main(port: str, speed: int):
+def speed_test(port: serial.Serial):
+    port.write(bytes([0]))
+    for x in range(256):
+        port.write(x.to_bytes(1, 'little'))
+
+def main(port: str, speed: int, speedtest):
     with serial.Serial(port, speed) as s:
+        if speedtest:
+            speed_test(s)
+
         while True:
             payload = input(">> ")
             send_raw_byte(s, payload)
@@ -24,6 +34,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--port', help='Serial port name', default="/dev/cu.SLAB_USBtoUART")
     parser.add_argument('-b', '--baudrate', help='Serial port baudrate', type=int, default=115200)
+    parser.add_argument('-s', '--speedtest', help='Performs a speed test', default=False, action='store_true')
+    parser.add_argument('-d', '--display', help='Print incoming bytes using a separate thread', default=False, action='store_true')
 
     args = parser.parse_args()
-    main(args.port, args.baudrate)
+    main(args.port, args.baudrate, args.speedtest)
