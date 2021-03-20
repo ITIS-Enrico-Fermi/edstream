26547f5639b65cceaaa7255d5dcbec9c0b95c6da
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2021-03-20 16:21:15 +0100

    Pixel rotation

---
+++ tools/rotate_pixel.py
@@ -0,0 +1,35 @@
+from PIL import Image
+import argparse
+import sys
+from io import BytesIO
+
+def main(use_stdin: bool, image: str):
+    if use_stdin:
+        stdin_img_bytes = BytesIO(sys.stdin.buffer.read())
+        img = Image.open(stdin_img_bytes)
+    else:
+        img = Image.open(image)
+
+    output = bytearray()
+
+    for bn in range(8): #block number
+        block: Image = img.crop((0, bn*8, 128, bn*8+8))
+        rblock: Image = block.rotate(-90, expand=True)
+        output += rblock.tobytes()
+
+    reassembled: Image = Image.frombytes('1', (64, 128), bytes(output))
+
+    img_byte_array = BytesIO()
+    reassembled.save(img_byte_array, format='bmp')
+    stdout_img_bytes: bytes = img_byte_array.getvalue()
+    sys.stdout.buffer.write(stdout_img_bytes)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--nostdin', default=True, action='store_false')
+    parser.add_argument('-i', '--image', default=None, required=False, type=str)
+    
+    args = parser.parse_args()
+
+    main(args.nostdin, args.image)
