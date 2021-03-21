e2f5cc093cc5ef47d1719b5d1ac2a2bf93e74c38
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2021-03-21 14:58:40 +0100

    Moved rotation in function New function GLCD_to_SSD1306 may be imported by other scripts

--- tools/rotate_pixel.py
+++ tools/rotate_pixel.py
@@ -14,22 +14,27 @@ import argparse
 import sys
 from io import BytesIO
 
+def GLCD_to_SSD1306(image: Image) -> Image:
+    output = bytearray()
+
+    for bn in range(8): #block number
+        block: Image = image.crop((0, bn*8, 128, bn*8+8))
+        rblock: Image = block.rotate(-90, expand=True)
+        output += rblock.tobytes()
+
+    reassembled: Image = Image.frombytes('1', (64, 128), bytes(output))
+    return reassembled
+
+
 def main(use_stdin: bool, image: str):
     if use_stdin:
         stdin_img_bytes = BytesIO(sys.stdin.buffer.read())
         img = Image.open(stdin_img_bytes)
     else:
         img = Image.open(image)
-
-    output = bytearray()
-
-    for bn in range(8): #block number
-        block: Image = img.crop((0, bn*8, 128, bn*8+8))
-        rblock: Image = block.rotate(-90, expand=True)
-        output += rblock.tobytes()
-
-    reassembled: Image = Image.frombytes('1', (64, 128), bytes(output))
-
+        
+    reassembled: Image = GLCD_to_SSD1306(img)
+    
     img_byte_array = BytesIO()
     reassembled.save(img_byte_array, format='bmp')
     stdout_img_bytes: bytes = img_byte_array.getvalue()
