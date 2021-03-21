f2f4d7241989a2e2604b1eba1152e01379b86f71
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-03-21 23:27:08 +0100

    async I/O

--- tools/uart_manager.py
+++ tools/uart_manager.py
@@ -4,8 +4,10 @@ Run this script in background.
 """
 import os
 import serial
-import argparse
 import logging
+import argparse
+import asyncio
+from typing import BinaryIO
 
 __authors__ = "5h wild nerds"
 
@@ -18,21 +20,31 @@ def create_fifo(path: str) -> None:
 def delete_fifo(path: str) -> None:
     os.unlink(path)
 
-def main(port: str, fifo_path: str) -> None:
+def read_fifo_coro(fifo: BinaryIO, uart: serial.Serial) -> None:
+    fifo_data: bytes = fifo.read()
+    print(fifo_data)
+    if fifo_data:
+        uart.write(fifo_data)
+        logging.info(f"FIFO: {fifo_data}")
+
+def read_uart_coro(fifo: BinaryIO, uart: serial.Serial) -> None:
+    uart_data: bytes = uart.read()
+    if uart_data:
+        fifo.write(uart_data)
+        logging.info(f"UART: {uart_data}")
+
+async def main(port: str, fifo_path: str) -> None:
     create_fifo(fifo_path)
     try:
         with serial.Serial(port=port, baudrate=115200, bytesize=8, parity='N', stopbits=1, timeout=0) as uart, open(fifo_path, 'r+b', 0) as fifo:
             fd = fifo.fileno()
             os.set_blocking(fd, False)
-            while True:
-                fifo_data: bytes = fifo.read()
-                if fifo_data:
-                    uart.write(fifo_data)
-                    logging.info(f"FIFO: {fifo_data}")
-                uart_data: bytes = uart.read()
-                if uart_data:
-                    fifo.write(uart_data)
-                    logging.info(f"UART: {uart_data}")
+            uart.flush()
+            loop = asyncio.get_event_loop()
+            loop.add_reader(fifo, read_fifo_coro, fifo, uart)
+            loop.add_reader(uart, read_uart_coro, fifo, uart)
+            never = asyncio.Future()
+            await never
     except:
         delete_fifo(fifo_path)
 
@@ -43,4 +55,4 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--port', help='Serial port name', type=str, default=SERIAL_PORT_NAME)
     parser.add_argument('-f', '--fifo', help='Named pipe (FIFO) for UART communication (debug_uart.py)', type=str, default='fifo')
     args = parser.parse_args()
-    main(args.port, args.fifo)
+    asyncio.run(main(args.port, args.fifo))
